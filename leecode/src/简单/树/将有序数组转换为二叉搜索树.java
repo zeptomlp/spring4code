59aa5a7cc34086de54a253ae2c62f46d000c8283
menglingpeng <menglingpeng@xy79.vip>
2021-05-13 16:23:09 +0800

    刷题

--- leecode/src/简单/树/将有序数组转换为二叉搜索树.java
+++ leecode/src/简单/树/将有序数组转换为二叉搜索树.java
@@ -6,4 +6,28 @@ package 简单.树;
  * @description:
  */
 public class 将有序数组转换为二叉搜索树 {
+
+    public static void main(String[] args) {
+        TreeNode treeNode = sortedArrayToBST(new int[]{-10, -3, 0, 5, 9});
+        System.out.println("123123123123");
+    }
+    public static TreeNode sortedArrayToBST(int[] nums) {
+        if (nums.length == 0) {
+            return null;
+        }
+        TreeNode treeNode = sortedArrayToBSTmath(nums, 0, nums.length - 1);
+        return treeNode;
+    }
+
+    public static TreeNode sortedArrayToBSTmath(int[] nums, int begin, int end) {
+        if (begin > end) {
+            return null;
+        }
+        int mid = (begin + end)/2;
+        TreeNode treeNode = new TreeNode(nums[mid]);
+        treeNode.left = sortedArrayToBSTmath(nums, begin, mid - 1);
+        treeNode.right = sortedArrayToBSTmath(nums, mid + 1, end);
+        return treeNode;
+    }
+
 }
