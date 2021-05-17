5992b88c8c9fed0cce8d8806d1f5dc7a537c3f41
menglingpeng <menglingpeng@xy79.vip>
2021-05-17 13:54:03 +0800

    刷题

--- leecode/src/简单/排序和搜索/第一个错误的版本.java
+++ leecode/src/简单/排序和搜索/第一个错误的版本.java
@@ -5,8 +5,23 @@ package 简单.排序和搜索;
  * @date: 2021/5/17
  * @description:
  */
+
+//说白了就是通过二分法找  100  50   75  .。。。。。。
 public class 第一个错误的版本 {
     public int firstBadVersion(int n) {
-        return 0;
+        int start = 1, end = n;
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (!isBadVersion(mid)) {
+                start = mid + 1;
+            } else {
+                end = mid;
+            }
+        }
+        return start;
+    }
+
+    private boolean isBadVersion(int mid) {
+        return false;
     }
 }
